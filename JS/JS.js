5b0db3be5c19b098466b14fb99b22ce667b2117a
Gustavo Nicolas Rodas <nicorodasrojas@gmail.com>
2026-07-01 19:31:20 -0300

    feat: codigo de JS "modo oscuro/claro" corregido y optimizado

--- JS/JS.js
+++ JS/JS.js
@@ -1,6 +1,14 @@
-const modoActivo = document.body.classList.contains("modo-oscuro");
 const btnModo = document.getElementById("btn-modo");
-btnModo.addEventListener("click",()=>{document.body.classList.toggle("modo-oscuro");
-    btnModo.textContent = modoActivo?"modo claro" : "modo oscuro"
 
+btnModo.addEventListener("click", () => {
+    
+    document.body.classList.toggle("modo-oscuro");
+    
+    const modoActivo = document.body.classList.contains("modo-oscuro");
+    
+    if (modoActivo) {
+        btnModo.textContent = "modo claro";
+    } else {
+        btnModo.textContent = "modo oscuro";
+    }
 });
